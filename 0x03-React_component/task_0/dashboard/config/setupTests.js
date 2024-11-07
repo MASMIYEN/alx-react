21c288ee76322a55be46cdd10646ec2d9a3a6e2b
Marouane Asmiyen <123839250+MASMIYEN@users.noreply.github.com>
2024-11-07 16:29:36 +0100

    major fixes

--- 0x03-React_component/task_0/dashboard/config/setupTests.js
+++ 0x03-React_component/task_0/dashboard/config/setupTests.js
@@ -1,4 +1,5 @@
+import '@testing-library/jest-dom';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-Enzyme.configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() });
