ae0ea5d0357f3d0318ac0ca1937b086ebadd868a
masmiyen <masmiyen@gmail.com>
2024-11-19 05:21:13 +0100

    nested

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,1 +1,7 @@
-#!/usr/bin/node
+import { fromJS} from 'immutable';
+
+export default function accessImmutableObject(object, array) {
+    const mapObj = fromJS(object);
+
+    return mapObj.getIn(array, undefined);
+}
