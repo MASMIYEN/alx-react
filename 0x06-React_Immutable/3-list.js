c5d328dfa30a40994ecdf3db1fc702137271d06f
masmiyen <masmiyen@gmail.com>
2024-11-21 17:34:48 +0100

    list2

--- 0x06-React_Immutable/3-list.js
+++ 0x06-React_Immutable/3-list.js
@@ -1,9 +1,8 @@
-import { list } from 'immutable';
-import { List } from './node_modules/immutable/dist/immutable';
+import { List } from 'immutable';
 
-export default function getListObject(array) {
-    return List(array);
+export function getListObject(array) {
+  return List(array);
 }
-export default function addElementToList(list, element) {
-    return list.push(element);
+export function addElementToList(list, element) {
+  return list.push(element);
 }
